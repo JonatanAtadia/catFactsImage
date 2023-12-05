b374d82250da876a914c69dd111a89cce5433a80
JonatanAtadia <jonatan.atadia@gmail.com>
2023-12-05 12:27:45 +0100

    Readme Updated and Finish the tasks

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-## Prueba tecnica para Juniors y Trainees de React en Live Coding.
+## Technical Test for React Juniors and Trainees in Live Coding.
 
 APIs:
 
 - Facts Random: https://catfact.ninja/fact
 - Imagen Random: https://cataas.com/cat/says/hello
 
-Recupera un hecho aleatorio de gatos de la primera API y muestra una imagen de un gato con la primera palabra del hecho recuperado usando la segunda API.
+Retrieve a random cat fact from the first API and display an image of a cat with the first word of the retrieved fact using the second API.
--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,51 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact';
+const CAT_ENDPOINT_IMAGE_URL =
+  'https://cataas.com/cat/says/${threeFirstWords}?fontSize=50&fontColor=red';
 
 function App() {
-  const [fact, setFact] = useState('');
+  const [fact, setFact] = useState();
+  const [imageUrl, setImageUrl] = useState();
 
+  // UseEffect to retrieve the quote when the page loads.
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then((res) => res.json())
-      .then((data) => setFact(data.fact));
+      .then((data) => {
+        const { fact } = data;
+        setFact(fact);
+      });
   }, []);
+
+  // UseEffect to retrieve the image each time we have a new quote.
+  useEffect(() => {
+    if (!fact) return;
+
+    const threeFirstWords = fact.split(' ', 3).join(' ');
+    const imageUrlWithWords = CAT_ENDPOINT_IMAGE_URL.replace(
+      '${threeFirstWords}',
+      encodeURIComponent(threeFirstWords)
+    );
+
+    fetch(imageUrlWithWords)
+      .then((res) => res)
+      .then((response) => {
+        const { url } = response;
+        setImageUrl(url);
+      });
+  }, [fact]);
 
   return (
     <main>
-      <h1>App de gatitos</h1>
+      <h1>Cat application with images and quotes</h1>
       {fact && <p>{fact}</p>}
+      {imageUrl && (
+        <img
+          src={imageUrl}
+          alt={`Image extracted using the first three words for ${fact}`}
+        />
+      )}
     </main>
   );
 }
